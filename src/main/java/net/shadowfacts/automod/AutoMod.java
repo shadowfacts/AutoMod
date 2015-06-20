2688b18d808905c403683c8d7afa37ca322ff180
Shadowfacts <me@shadowfacts.net>
2015-06-20 13:56:15 -0400

    Add main mod class

---
+++ src/main/java/net/shadowfacts/automod/AutoMod.java
@@ -0,0 +1,48 @@
+package net.shadowfacts.automod;
+
+import cpw.mods.fml.client.registry.ClientRegistry;
+import cpw.mods.fml.common.FMLCommonHandler;
+import cpw.mods.fml.common.Mod;
+import cpw.mods.fml.common.event.FMLPreInitializationEvent;
+import cpw.mods.fml.common.event.FMLServerStartingEvent;
+import net.minecraft.client.settings.KeyBinding;
+import net.shadowfacts.shadowapi.command.CommandHandler;
+import net.shadowfacts.shadowapi.config.ConfigManager;
+import net.shadowfacts.shadowapi.util.Log;
+import org.lwjgl.input.Keyboard;
+
+/**
+ * @author shadowfacts
+ */
+@Mod(modid = AutoMod.id, name = AutoMod.name, version = AutoMod.version)
+public class AutoMod {
+
+	public static final String id = "automod";
+	public static final String name = "AutoMod";
+	public static final String version = "@VERSION@";
+
+	public static Log logger = new Log("AutoMod");
+
+	public static KeyBinding mainKeybind;
+
+	public static boolean isEnabled = false;
+
+	@Mod.EventHandler
+	public void preInit(FMLPreInitializationEvent event) {
+		ConfigManager.instance.configDirPath = event.getModConfigurationDirectory().getAbsolutePath();
+		ConfigManager.instance.register("AutoMod", Configuration.class);
+
+		mainKeybind = new KeyBinding("key.enfusion.main", Keyboard.KEY_Y, "key.enfusion.category");
+		ClientRegistry.registerKeyBinding(mainKeybind);
+
+		FMLCommonHandler.instance().bus().register(new FMLEventHandler());
+	}
+
+	@Mod.EventHandler
+	public void serverLoad(FMLServerStartingEvent event) {
+		CommandHandler.registerSubCommand(CommandAutoMod.instance);
+		CommandHandler.initCommands(event);
+	}
+
+
+}
