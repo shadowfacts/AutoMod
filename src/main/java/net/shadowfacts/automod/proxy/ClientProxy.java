6834b516fe9f3ffcfc0c839f3ab839ab275dc140
Shadowfacts <me@shadowfacts.net>
2015-06-20 21:48:05 -0400

    Add displayGuiScreen method

--- src/main/java/net/shadowfacts/automod/proxy/ClientProxy.java
+++ src/main/java/net/shadowfacts/automod/proxy/ClientProxy.java
@@ -1,7 +1,15 @@
 package net.shadowfacts.automod.proxy;
 
+import net.minecraft.client.Minecraft;
+import net.minecraft.client.gui.GuiScreen;
+
 /**
  * @author shadowfacts
  */
 public class ClientProxy extends CommonProxy {
+
+	@Override
+	public void displayGuiScreen(GuiScreen gui) {
+		Minecraft.getMinecraft().displayGuiScreen(gui);
+	}
 }
--- src/main/java/net/shadowfacts/automod/proxy/CommonProxy.java
+++ src/main/java/net/shadowfacts/automod/proxy/CommonProxy.java
@@ -1,7 +1,14 @@
 package net.shadowfacts.automod.proxy;
 
+import net.minecraft.client.gui.GuiScreen;
+
 /**
  * @author shadowfacts
  */
 public class CommonProxy {
+
+	public void displayGuiScreen(GuiScreen gui) {
+
+	}
+
 }
